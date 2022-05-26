f60f839e2d5296c50aa929a6e23c0ba19fdc2629
CAROCARMONA <92407855+CAROCARMONA@users.noreply.github.com>
2022-05-26 10:55:50 -0500

    "2"

--- APP/methods.cs
+++ APP/methods.cs
@@ -21,24 +21,23 @@ namespace APP
         // forecast function
         public List<double> Forecast()
         {
-          
-            List<double> TRIMESTERS = new List<double>() { 1,2,3,4,5,6,7,8,9,10,11,12};
 
+            List<double> TRIMESTERS = new List<double>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
             List<double> forecast = new List<double>();
-            List<double> Iseasonal = new List<double>();//  seasonal index
-            List<double> SeasonallyAdjust = new List<double>();//   seasonally adjust
-
+     
+            double[] Iseasonal = new double[Sales.Count]; //  seasonal indEX
+            double[] SeasonallyAdjust = new double[Sales.Count];//   seasonally adjust
 
             double totalx = 0;//  class mark
             double totaly = 0;// total sales
             double totalDES = 0;// total and seasonally adjusted
 
-            List<double> XX = new List<double>();
-            List<double> XY = new List<double>();
-           
+      
+            double[] XX = new double[Sales.Count];
+            double[] XY= new double[Sales.Count];
+
             double totalXX = 0;//total the sales XX
             double totalXY = 0;//total de sales XY
-
 
 
             for (int i = 0; i < Sales.Count; i++)
@@ -49,37 +48,27 @@ namespace APP
 
             double avarageY = totaly / Sales.Count;// average quarterly sales
           
-            double[] I = new double[Sales.Count];
            
           for (int i = 0; i < Sales.Count; i = i + 4)
             {
                 //               Specific quarterly average            iseasonal
-                I[i] = ((Sales[0] + Sales[4] + Sales[8]) / 3) / avarageY ;
-                I[i + 1] = ((Sales[1] + Sales[5] + Sales[9]) / 3) / avarageY;
-                I[i + 2] = ((Sales[2] + Sales[6] + Sales[10]) / 3) / avarageY;
-                I[i + 3] = ((Sales[3] + Sales[7] + Sales[11]) / 3) / avarageY;
+                Iseasonal[i] = ((Sales[0] + Sales[4] + Sales[8]) / 3) / avarageY ;
+                Iseasonal[i + 1] = ((Sales[1] + Sales[5] + Sales[9]) / 3) / avarageY;
+                Iseasonal[i + 2] = ((Sales[2] + Sales[6] + Sales[10]) / 3) / avarageY;
+                Iseasonal[i + 3] = ((Sales[3] + Sales[7] + Sales[11]) / 3) / avarageY;
 
             }
 
-            double[] S = new double[Sales.Count];
-            double[] X= new double[Sales.Count];
-            double[] Y = new double[Sales.Count];
-
             for (int i = 0; i < Sales.Count; i++)
             {
-                // adding to seasonal index list
-                Iseasonal.Add(I[i]);
 
-                S[i] = Sales[i] / Iseasonal[i];
-                SeasonallyAdjust.Add(S[i]);
+                SeasonallyAdjust[i] = Sales[i] / Iseasonal[i];
                 totalDES += SeasonallyAdjust[i];// sum of all the seasonally adjusted
 
                 // x a la 2    y    XY
-                X[i] = TRIMESTERS[i] * TRIMESTERS[i];
-                XX.Add(X[i]);
+                XX[i] = TRIMESTERS[i] * TRIMESTERS[i];
                 totalXX += XX[i];
-                Y[i] = TRIMESTERS[i] * SeasonallyAdjust[i];
-                XY.Add(Y[i]);
+                XY[i] = TRIMESTERS[i] * SeasonallyAdjust[i];
                 totalXY += XY[i];
             }
             //regression equation a and b
